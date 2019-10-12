2677666517ee02294845189ae7b173c5c20abc0a
lnfinite Monkeys <lnfinitemonkeys@tuta.io>
2019-10-12 13:43:11 +0100

    Fix emacs config tangling

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,14 +1,9 @@
-;;; An example config file to get you started with Emacs.
-;;; It uses use-package for automatic package installation and management,
-;;; it maintains the familiar-to-many Ctrl-c for copy, Ctrl-x for cut, Ctrl-v for paste.
-
 ;; Windows performance tweaks
 (when (boundp 'w32-pipe-read-delay)
   (setq w32-pipe-read-delay 0))
 ;; Set the buffer size to 64K on Windows (from the original 4K)
 (when (boundp 'w32-pipe-buffer-size)
   (setq irony-server-w32-pipe-buffer-size (* 64 1024)))
-
 
 ;;;;;;;;;;;;;;;;; Setup package stuff
 (require 'package)
@@ -27,25 +22,11 @@
   (require 'use-package)
   (setq use-package-always-ensure t))
 
-;; Haskell mode provides syntax highlighting and other goodies
+(use-package org)
 
-;; Use General for easy key binding definitions
-;; Load up timelines-mode, the star of the evening
-(load "~/.emacs.d/timelines-mode.el")
-
-;;
-;; ~~~ Change this if you cloned the TimeLines source repo in any other path ~~~
-;;
-(setq timelines-path "~/timelines")
-
-;; Snippets = less typing = good
-
-
-(defvar literate-config "~/.config/emacs/config.org")
-(org-babel-load-file (expand-file-name literate-config))
-
-
-(setq initial-major-mode 'lisp-interaction-mode)
+(defconst literate-config (expand-file-name "~/.config/emacs/config.org"))
+(when (file-exists-p literate-config)
+  (org-babel-load-file (expand-file-name literate-config)))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -54,7 +35,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (evil nord-theme ido-vertical-mode monokai-theme haskell-mode use-package))))
+    (evil nord-theme ido-vertical-mode haskell-mode use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
