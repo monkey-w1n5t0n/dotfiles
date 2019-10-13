984a086c9bce376efa9f870555e2541520303b1a
lnfinite Monkeys <lnfinitemonkeys@tuta.io>
2019-10-13 22:35:28 +0100

    Temporary fix emacs tangling, add scripts

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -25,17 +25,23 @@
 (use-package org)
 
 (defconst literate-config (expand-file-name "~/.config/emacs/config.org"))
+(defconst tangled-config "config.el")
 (when (file-exists-p literate-config)
-  (org-babel-load-file (expand-file-name literate-config)))
+  (org-babel-tangle-file literate-config tangled-config)
+  (org-babel-load-file literate-config))
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
  '(package-selected-packages
    (quote
-    (evil nord-theme ido-vertical-mode haskell-mode use-package))))
+    (solaire-mode doom-themes treemacs-evil treemacs intero doom-modeline modeline smart-mode-line evil nord-theme ido-vertical-mode haskell-mode use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
