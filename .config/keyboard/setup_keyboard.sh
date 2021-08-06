2bdaeaedbebdeda1a40738fa902a3cc382d2c24a
w1n5t0n <lm-w1n5t0n@protonmail.com>
2021-08-06 05:18:34 +0200

    Add slurp/barf to emacs, fix Bluetooth keyboard layout

--- .config/keyboard/setup_keyboard.sh
+++ .config/keyboard/setup_keyboard.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/sh
+
+# Languages
 setxkbmap 'us,gr' -variant 'dvorak,simple' -option grp:alt_shift_toggle
+setxkbmap -device 26 -layout 'us,gr' -variant 'dvorak,simple' -option grp:alt_shift_toggle
+
+# Misc keyboard stuff
+
+#           delay  speed
+xset r rate 200    40
