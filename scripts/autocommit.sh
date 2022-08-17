ea86ce2bdee8692ff2b0db3a47cfebf5980931a2
w1n5t0n <lm-w1n5t0n@protonmail.com>
2022-08-17 10:21:10 +0100

    Change notes(-autocommit) to notes(-autocommit)-bare-repo

--- scripts/autocommit.sh
+++ scripts/autocommit.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-/usr/bin/git --git-dir=$HOME/.local/notes-autocommit --work-tree=$HOME/notes add .
-/usr/bin/git --git-dir=$HOME/.local/notes-autocommit --work-tree=$HOME/notes commit -am "Automatic Commit: `date +%F-%T`"
+/usr/bin/git --git-dir=$HOME/.local/notes-autocommit-bare-repo --work-tree=$HOME/notes add .
+/usr/bin/git --git-dir=$HOME/.local/notes-autocommit-bare-repo --work-tree=$HOME/notes commit -am "Automatic Commit: `date +%F-%T`"
