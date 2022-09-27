868a7915793dd769bf3cc41d3cc0787687b4287b
w1n5t0n <lm-w1n5t0n@protonmail.com>
2022-09-27 01:36:01 +0100

    Add shell and zsh configs, change dotfile git path to .local/dotfiles-git

---
+++ .config/shell/alias.sh
@@ -0,0 +1,23 @@
+git=/usr/bin/git
+
+alias notes="$git \
+    --git-dir=$NOTES_GIT_DIR \
+    --work-tree=$NOTES_WORK_TREE"
+alias notes_ac="$git \
+    --git-dir=$NOTES_AUTOGIT_DIR \
+    --work-tree=$NOTES_WORK_TREE"
+alias n=notes
+alias na=notes_ac
+
+alias dotfiles="$git --git-dir=$DOTFILES_GIT_DIR --work-tree=$HOME"
+alias dot=dotfiles
+
+# TODO not working?
+alias cdsrc=cd $SRC_DIR
+alias cdscr=cd $SCRIPT_DIR
+
+# Taken from the manjaro zsh config
+alias cp="cp -i"                                                # Confirm before overwriting something
+alias df='df -h'                                                # Human-readable sizes
+alias free='free -m'                                            # Show sizes in MB
+alias gitu='git add . && git commit && git push'
---
+++ .config/shell/env.sh
@@ -0,0 +1,36 @@
+export EDITOR="emacsclient -c &"
+export BROWSER=/usr/bin/firefox
+export FILE=/usr/bin/pcmanfm
+
+export DOOM_DIR="$HOME/.emacs.d"
+export DOOM_USER_DIR="$HOME/.config/doom"
+
+export DOTFILES_GIT_DIR="$HOME/.local/dotfiles-git"
+
+# NOTES
+export NOTES_DIR="$HOME/notes"
+export ORG_DIR="$NOTES_DIR/org"
+export ORG_ROAM_DIR="$NOTES_DIR/org-roam"
+## Notes Git
+export NOTES_GIT_DIR="$HOME/.local/notes-git"
+export NOTES_AUTOGIT_DIR="$HOME/.local/notes-autogit"
+export NOTES_WORK_TREE="$NOTES_DIR"
+
+# src & scripts
+export SRC_DIR="$HOME/src"
+export SCRIPT_DIR="$HOME/scripts"
+
+# EMACS
+export EMACS_DIR="$HOME/emacs"
+export DOOM_DIR="$CONFIG_DIR/doom"
+#PATH_prepend($EMACS_DIR/bin)
+
+# i3
+export I3_CONFIG="$CONFIG_DIR/i3/config"
+# SXHKD
+export SXHKD_CONFIG="$CONFIG_DIR/sxhkd/sxhkdrc"
+
+export QT_QPA_PLATFORMTHEME="qt5ct"
+export GTK2_RC_FILES="$HOME/.gtkrc-2.0"
+
+export JAVA_HOME="/usr/lib/jvm/java-18-openjdk"
