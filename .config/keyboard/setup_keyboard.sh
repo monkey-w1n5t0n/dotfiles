ae65f61fd665bba2d874ced71af332ff02157c38
w1n5t0n <lm-w1n5t0n@protonmail.com>
2022-12-25 06:39:11 -0200

    stable but unexciting setup on michelle

--- .config/keyboard/setup_keyboard.sh
+++ .config/keyboard/setup_keyboard.sh
@@ -4,9 +4,6 @@
 setxkbmap 'us(dvorak), gr(simple)' \
   -option grp:alt_shift_toggle
 
-setxkbmap -device 21 -layout 'us,gr' \
-  -option grp:alt_shift_toggle
-
 # Misc keyboard stuff
 
 #           delay  speed
